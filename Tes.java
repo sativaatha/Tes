17b82e8890773330d6102d4beabb01c3a1bac1e5
sativaatha <sativa_azzahra_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:12:50 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,7 @@
+public class Tes { 
+    public static void main(String[] args) {
+  String nama = "sativa";
+  String kelas = "X RPL 5";
+  system.out.print("Halo, nama saya "+ sativa + "dari kelas " + X RPL 5);   
+    }
+}
